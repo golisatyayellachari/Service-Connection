51fe97fb9a0c0952686ccbb5e82519a38115d8a7
sgoli <sgoli@osius.com>
2023-06-19 15:01:28 +0530

    updated setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Service Connection',
+    version='1.0.1',
+    description='To estbilash connection between micro services',
+    url='https://github.com/golisatyayellachari/Service-Connection.git',
+    author='Satya Goli',
+    author_email='sgoli@osius.com',
+    license='unlicense',
+    packages=['serviceconnection'],
+    zip_safe=False
+)
